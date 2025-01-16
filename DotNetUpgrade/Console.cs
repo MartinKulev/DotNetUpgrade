0c342b92ca94c489832f396f8a29bdf63aff3508
Martin Kulev <56129939+MartinKulev@users.noreply.github.com>
2025-01-16 21:44:02 +0200

    [1.0.3] Initial commit

---
+++ DotNetUpgrade/Console.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetUpgrade
+{
+    public static class Console
+    {
+        public static void WriteLineRange<T>(IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ DotNetUpgrade/README.md
@@ -0,0 +1,5 @@
+# DotNetUpgrade
+A NuGet package containing useful features and improvements that don't exist in .Net 9.
+
+## Download
+You can find the package [here](https://www.nuget.org/packages/DotNetUpgrade/) or by searching "DotNetUpgrade" in the NuGet store
