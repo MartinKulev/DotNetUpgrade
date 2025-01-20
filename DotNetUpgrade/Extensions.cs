fda096e11f89dd992c47e1a046bdd77c226923dd
Martin Kulev <56129939+MartinKulev@users.noreply.github.com>
2025-01-20 21:06:01 +0200

    [1.0.4] Added IsNullOrEmpty method

---
+++ DotNetUpgrade/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetUpgrade
+{
+    public static class Extensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
+        {
+            return collection == null || !collection.Any();
+        }
+    }
+}
