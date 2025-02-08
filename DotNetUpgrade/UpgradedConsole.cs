2604c11e983482b957e3831b61da808aa03380bb
Martin Kulev <56129939+MartinKulev@users.noreply.github.com>
2025-02-08 21:24:41 +0200

    [1.0.6] Changed Console to UpgradedConsole to prevent ambigious

--- DotNetUpgrade/UpgradedConsole.cs
+++ DotNetUpgrade/UpgradedConsole.cs
@@ -1,12 +1,12 @@
 ﻿namespace DotNetUpgrade
 {
-    public static class Console
+    public static class UpgradedConsole
     {
         public static void WriteLineRange<T>(IEnumerable<T> collection)
         {
             foreach (var item in collection)
             {
-                System.Console.WriteLine(item);
+                Console.WriteLine(item);
             }
         }
     }
