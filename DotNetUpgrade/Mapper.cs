d194bef8f488cf168892afb03553161bb95168ba
Martin Kulev <56129939+MartinKulev@users.noreply.github.com>
2025-02-08 21:01:53 +0200

    [1.0.5] Added Mapper of properties and fields

---
+++ DotNetUpgrade/Mapper.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+
+namespace DotNetUpgrade
+{
+    public static class Mapper
+    {
+        public static TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
+        {
+            if (source == null || destination == null)
+            {
+                throw new ArgumentNullException("Source or destination cannot be null.");
+            }
+
+            PropertyInfo[] sourceProperties = typeof(TSource).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            FieldInfo[] sourceFields = typeof(TSource).GetFields(BindingFlags.Public | BindingFlags.Instance);
+            PropertyInfo[] destinationProperties = typeof(TDestination).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (PropertyInfo sourceProp in sourceProperties)
+            {
+                var destProp = destinationProperties
+                    .FirstOrDefault(dp => dp.Name.Equals(sourceProp.Name, StringComparison.OrdinalIgnoreCase) &&
+                                          dp.PropertyType == sourceProp.PropertyType);
+
+                if (destProp != null && destProp.CanWrite)
+                {
+                    destProp.SetValue(destination, sourceProp.GetValue(source));
+                }
+            }
+
+            foreach (FieldInfo sourceField in sourceFields)
+            {
+                var destProp = destinationProperties
+                    .FirstOrDefault(dp => dp.Name.Equals(sourceField.Name, StringComparison.OrdinalIgnoreCase) &&
+                                          dp.PropertyType == sourceField.FieldType);
+
+                if (destProp != null && destProp.CanWrite)
+                {
+                    destProp.SetValue(destination, sourceField.GetValue(source));
+                }
+            }
+
+            return destination;
+        }
+    }
+}
